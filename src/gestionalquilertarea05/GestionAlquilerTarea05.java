d393ec12ffa93249db3b4caaeb41b8a6dce1bd14
FelipeRodriguezLorente <>
2018-01-20 11:54:41 +0100

    Se crea el paquete utilidades con la clase Entrada.

---
+++ src/gestionalquilertarea05/GestionAlquilerTarea05.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionalquilertarea05;
+
+/**
+ *
+ * @author Felipillo
+ */
+public class GestionAlquilerTarea05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
