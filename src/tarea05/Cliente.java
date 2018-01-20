81793382f9f0a68817748a001a657727d793c4df
FelipeRodriguezLorente <>
2018-01-20 17:10:35 +0100

    Se crea la clase Cliente con los atributos y visibilidad correcta.

---
+++ src/tarea05/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+/**
+ *
+ * @author Felipillo
+ */
+public class Cliente {
+    
+    private String nombre;
+    private String dni;
+    private String direccion;
+    private String localidad;
+    private String codigoPostal;
+    private int identificador;
+    private static int numClientes;
+    
+}
