baa0d3b2b61428446f0d4c369329c128c6f86392
FelipeRodriguezLorente <>
2018-01-20 20:15:47 +0100

    Se crea la clase Turismo con sus atributos.

---
+++ src/tarea05/Turismo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+/**
+ *
+ * @author Felipillo
+ */
+public class Turismo {
+    
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private int cilindrada;
+    private boolean disponible;
+    
+}
