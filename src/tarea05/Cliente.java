cdcc832d0237db6dd19385bd3169cf31080c6698
FelipeRodriguezLorente <>
2018-01-20 17:36:28 +0100

    Se crea un constructor con cinco parametros de la clase lanzando excepcion si el dni o el codigo postal no son correctos. Para comprobarlos se crean un metodo para cada uno

--- src/tarea05/Cliente.java
+++ src/tarea05/Cliente.java
@@ -5,12 +5,15 @@
  */
 package tarea05;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Felipillo
  */
 public class Cliente {
-    
+
     private String nombre;
     private String dni;
     private String direccion;
@@ -18,5 +21,42 @@ public class Cliente {
     private String codigoPostal;
     private int identificador;
     private static int numClientes;
-    
+
+    public Cliente(String nombre, String dni, String direccion, String localidad, String codigoPostal) {
+
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.localidad = localidad;
+
+        if (compruebaDni(dni)) {
+            this.dni = dni;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("El DNI introducido no es correcto");
+        }
+        if (compruebaCodigoPostal(codigoPostal)) {
+            this.codigoPostal = codigoPostal;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("El Codigo postal introducido no es correcto");
+        }
+
+    }
+
+    private boolean compruebaDni(String dni) {
+        Pattern patron = Pattern.compile("[0-9]{8}[a-zZ-a]{1}");
+        Matcher emparejador;
+        emparejador = patron.matcher(dni);
+
+        return emparejador.matches();
+
+    }
+
+    private boolean compruebaCodigoPostal(String codigoPostal
+    ) {
+        Pattern patron = Pattern.compile("([0-9] {5})");
+        Matcher emparejador;
+        emparejador = patron.matcher(codigoPostal);
+
+        return emparejador.matches();
+
+    }
 }
