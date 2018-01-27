e259f18afecc1d3dc46bd2bb7582702c450ea4c5
FelipeRodriguezLorente <>
2018-01-27 20:42:19 +0100

    Se crea la clase Alquiler Vehiculos con sus atributos.

---
+++ src/tarea05/AlquilerVehiculos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+/**
+ *
+ * @author Felipillo
+ */
+public class AlquilerVehiculos {
+    private final int MAX_TURISMOS=100;
+    private final int MAX_CLIENTES=100;
+    private final int MAX_ALQUILERES=100;
+    
+    
+}
