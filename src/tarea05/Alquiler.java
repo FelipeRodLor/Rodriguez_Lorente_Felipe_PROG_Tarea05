4740ba28028dd232fa870caa0c217362e6f2b2d1
FelipeRodriguezLorente <>
2018-01-27 20:11:57 +0100

    Se crea el metodo toString en la clase Alquiler.

--- src/tarea05/Alquiler.java
+++ src/tarea05/Alquiler.java
@@ -14,59 +14,59 @@ import java.util.Date;
  * @author Felipillo
  */
 public class Alquiler {
-    
+
     private Date fecha;
     private int dias;
-    private final SimpleDateFormat FORMATO_FECHA;
+    private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat ("dd/MM/yyyy HH;mm");
     private final int MS_DIA = 1000 * 60 * 60 * 24;
     private final double PRECIO_DIA = 30;
     private Cliente cliente;
     private Turismo turismo;
-    
+
     public Alquiler(Cliente cliente, Turismo turismo) {
-        
+
         this.cliente = cliente;
         this.turismo = turismo;
         fecha = new Date();
         dias = 0;
         turismo.setDisponible(false);
-        
+
     }
-    
+
     public Date getFecha() {
         return fecha;
     }
-    
+
     public int getDias() {
         return dias;
     }
-    
-    public double getPrecio() {
-        return PRECIO_DIA * dias + turismo.getCilindrada()/100;
-    }
-    
+
     public Cliente getCliente() {
         return cliente;
     }
-    
+
     public Turismo getTurismo() {
         return turismo;
     }
-    
+
     public void close() {
         Date ahora = new Date();
         dias = difDias(ahora, fecha);
         turismo.setDisponible(true);
-        
+
     }
-    
+
     private int difDias(Date fechaFin, Date fechaInicio) {
         long milisegundos = fechaFin.getTime() - fechaInicio.getTime();
         long diasAlq = milisegundos / MS_DIA;
         return (int) diasAlq + 1;
-        
+
     }
-    
-    
-    
+    public double getPrecio() {
+        return PRECIO_DIA * dias + turismo.getCilindrada() / 100;
+    }
+    public String toString(){
+        return ("Fecha de alquiler; " + FORMATO_FECHA.format(fecha) + "Cliente; " + cliente + " Vehiculo aquilado; " + turismo);
+    }
+
 }
